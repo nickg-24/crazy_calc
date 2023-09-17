ec6f1e83c31e6699d17bad0c01e9d1ef9347d147
Nick G <nickgei123@gmail.com>
2023-09-17 15:38:43 -0400

    method, version, header checks work. need to check the request uri

--- code/parser/parser_1.py
+++ code/parser/parser_1.py
@@ -13,9 +13,17 @@ def validate(req_str):
         # check if blank
         if req_str == '':
             return 400
+        
+        # split the request into headers and body, if both are present
+        parts = re.split(r'\r\n\r\n|\n\n', req_str, maxsplit=1)
+        if len(parts) == 2:
+            headers, body = parts
+        else:
+            headers = parts[0]
+            body = ''
+
         # split on newlines and get rid of the \r if its there        
-        lines = req_str.split('\n')
-        normalized_lines = [line.rstrip('\r') for line in lines]
+        normalized_lines = [line.rstrip('\r') for line in headers.split('\n')]
 
         # check num of parts in top_line
         top_line = normalized_lines.pop(0).split()
@@ -26,18 +34,29 @@ def validate(req_str):
         uri = top_line[1]
         version = top_line[2]
         if method not in methods:
-            return 400
+            return 405
         if version not in httpVersions:
-            return 400
+            return 505
         
         # check headers
-
+        for line in normalized_lines:
+            if ':' not in line:
+                return 400
+            head_name, head_value = line.split(':', 1)
+            # check that head_name has no spaces
+            for char in head_name:
+                if char.isspace():
+                    return 400
 
         # check body
         
+
+        return 200
+    
     # server error
     except Exception as e:
         print(f"Exception: {e}")
+        return 500  # HTTP 500 Internal Server Error
 
 def main():
     path_2_req = sys.argv[1]
