95ceca97cd68b9c8ee5f7b10f46adcc31612c017
Nick G <nickgei123@gmail.com>
2023-10-26 17:24:53 -0400

    issues with executing from other folders

--- code/src/php_exec.py
+++ code/src/php_exec.py
@@ -32,7 +32,7 @@ echo "test=1" | php-cgi
 '''
 
 # sets up the environment variables 
-def env_setup(method, script_filename, query_string="", post_body=""):
+def execute_php(method: str, script_filename: str, query_string: str="", post_body: str=""):
     # sets up GET env variables seen above
     if method == "GET":
         env = {
@@ -45,7 +45,7 @@ def env_setup(method, script_filename, query_string="", post_body=""):
     if method == "POST":
         env = {
             'REDIRECT_STATUS' : '0',
-            'SCRIPT_FILENAME' : 'cgi-test2.php',
+            'SCRIPT_FILENAME' : script_filename,
             'REQUEST_METHOD' : 'POST',
             'GATEWAY_INTERFACE' : 'CGI/1.1',
             'CONTENT_TYPE' : 'application/x-www-form-urlencoded'
@@ -53,6 +53,27 @@ def env_setup(method, script_filename, query_string="", post_body=""):
 
         if post_body:
             env['CONTENT_LENGTH'] = str(len(post_body))
-        CONTENT_LENGTH="6" \
-        echo "test=1" | php-cgi
 
+    # make the shell command
+    proc = subprocess.Popen(
+        ['php-cgi'],
+        stdin=subprocess.PIPE, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE,
+        env= env
+    )
+
+    if post_body:
+        stdout_data, stderr_data = proc.communicate(input=post_body.encode())
+    else:
+        stdout_data, stderr_data = proc.communicate()
+    print(env)
+    print(stdout_data)
+
+# for testing
+def main():
+    
+    execute_php("GET","cgi-test2.php","<parameter1>=<value1>&<parameter2>=<value2>&[...]&<parameterN>=<valueN>")
+
+if __name__ == "__main__":
+    main()
