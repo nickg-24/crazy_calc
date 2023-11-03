b822e3b0ea9ab1ddc5c10902d062053411390ec3
Nick G <nickgei123@gmail.com>
2023-11-03 13:54:10 -0400

    error handling calc.php

--- code/www/calc.php
+++ code/www/calc.php
@@ -1,4 +1,19 @@
 <?php
+// Set a custom error handler
+set_error_handler(function($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+});
+
+register_shutdown_function(function(){
+    $error = error_get_last();
+    if ($error !== NULL) {
+        echo '<h1>500 Error: Internal Server Error</h1>';
+    }
+});
+
+$result = null;
+$visitor_name = null;
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['visitor_name'], $_POST['number1'], $_POST['operation'], $_POST['number2'])) {
         $visitor_name = $_POST['visitor_name'];
@@ -8,18 +23,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // calc string
         $calculation = $number1 . ' ' . $operation . ' ' . $number2;
-        eval("\$result = $calculation;");
-
+        // Handle eval in a try-catch block
+        try {
+            eval("\$result = $calculation;");
+        } catch (Throwable $e) { // Catch any errors or exceptions
+            echo '<h1>500 Error: Internal Server Error</h1>';
+            exit; // Stop script execution after an error
+        }
     } else {
-        echo "Invalid input.";
+        echo '<h1>500 Error: Invalid Input</h1>';
+        exit; // Stop script execution if input is invalid
     }
 } else {
-    echo "Please use the form to submit a calculation.";
+    echo '<h1>500 Error: Please use the form to submit a calculation.</h1>';
+    exit; // Stop script execution if the method is not POST
 }
 ?>
 
-<h1>Welcome <?php echo $visitor_name;?> </h1>
+<h1>Welcome <?php echo $visitor_name; ?></h1>
 <h2>Thanks for calculating with us! Your results can be found below. Please come again!</h2>
-<p>Result: <?php echo $result?> </p>
-
-
+<p>Result: <?php echo $result; ?></p>
