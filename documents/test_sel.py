840be56aa432833f3bb5095f44b15308892acfe7
Nick G <nickgei123@gmail.com>
2023-11-16 09:08:48 -0500

    selenium env setup

--- documents/test_sel.py
+++ documents/test_sel.py
@@ -1,20 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up the service object to point to where the chromedriver executable is located
-service = Service(executable_path='./chromedriver-linux64/chromedriver')
+# Create a WebDriver instance using Chrome
+driver = webdriver.Chrome()
 
+# Get the page
+driver.get("https://www.python.org")
 
-# Create a new instance of the Chrome driver
-driver = webdriver.Chrome(service=service)
-
-
-# Navigate to a webpage
-driver.get('https://www.python.org')
-
-# Example action: print the title of the page
-print(driver.title)
-
-# Quit the driver to close the browser
+# Don't forget to quit the WebDriver when you're done
 driver.quit()
+
